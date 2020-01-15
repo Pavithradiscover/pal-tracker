f8dcefd28308e2c521f681192759369050f28f11
Pavithradiscover <pavithravj5@outlook.com>
2020-01-15 10:59:53 -0700

    JDBC template changes

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -20,4 +24,20 @@ public class PalTrackerApplication {
     TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
     }
+
+
+    @Bean
+    DataSource dataSource(){
+        MysqlDataSource dataSource= new MysqlDataSource();
+        dataSource.setURL(System.getenv("SPRING_DATASOURCE_URL"));
+        return  dataSource;
+    }
+    @Bean
+    @Primary
+    TimeEntryRepository jdbcTimeEntryRepository()
+    {
+        return new JdbcTimeEntryRepository(dataSource());
+    }
+
+
 }
